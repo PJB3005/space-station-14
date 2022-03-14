cae1eed134f54b5e9248624927320b8d50732d1a
Pieter-Jan Briers <pieterjan.briers+git@gmail.com>
2022-03-14 01:16:12 +0100

    Ptr<T> helper type.
    To make it somewhat saner to use pointers in generics.

---
+++ Robust.Shared/Utility/Ptr.cs
@@ -0,0 +1,12 @@
+﻿namespace Robust.Shared.Utility;
+
+/// <summary>
+/// Wrapper type around pointers so I can use them as generic args without erasing it to nint.
+/// </summary>
+internal unsafe struct Ptr<T> where T : unmanaged
+{
+    public T* P;
+
+    public static implicit operator T*(Ptr<T> p) => p.P;
+    public static implicit operator Ptr<T>(T* p) => new() { P = p };
+}
