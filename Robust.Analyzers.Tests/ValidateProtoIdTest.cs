f75eca27b60874a92296a37183506d99a32ec5f6
PJB3005 <pieterjan.briers+git@gmail.com>
2025-02-24 19:08:22 +0100

    Add analyzer to check redundant ValidatePrototypeId attributes.
    Only one hit in content, surprisingly.

---
+++ Robust.Analyzers.Tests/ValidateProtoIdTest.cs
@@ -0,0 +1,77 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.CodeAnalysis.Testing;
+using NUnit.Framework;
+using VerifyCS =
+    Microsoft.CodeAnalysis.CSharp.Testing.CSharpAnalyzerVerifier<Robust.Analyzers.ValidateProtoIdAnalyzer, Microsoft.CodeAnalysis.Testing.DefaultVerifier>;
+
+namespace Robust.Analyzers.Tests;
+
+[Parallelizable(ParallelScope.All | ParallelScope.Fixtures)]
+[TestFixture]
+[TestOf(typeof(ValidateProtoIdAnalyzer))]
+public sealed class ValidateProtoIdTest
+{
+    private const string Defs = """
+        using System;
+
+        namespace Robust.Shared.Serialization.Manager.Attributes
+        {
+            [AttributeUsage(AttributeTargets.Field)]
+            public sealed class ValidatePrototypeIdAttribute<T> : Attribute
+            {
+            }
+        }
+
+        namespace Robust.Shared.Prototypes
+        {
+            public readonly struct ProtoId<T>(string Id);
+        }
+        """;
+
+    private static Task Verifier(string code, params DiagnosticResult[] expected)
+    {
+        var test = new CSharpAnalyzerTest<ValidateProtoIdAnalyzer, DefaultVerifier>()
+        {
+            TestState =
+            {
+                Sources = { code }
+            },
+        };
+
+        test.TestState.Sources.Add(("Deps.cs", Defs));
+
+        // ExpectedDiagnostics cannot be set, so we need to AddRange here...
+        test.TestState.ExpectedDiagnostics.AddRange(expected);
+
+        return test.RunAsync();
+    }
+
+    [Test]
+    public async Task Test()
+    {
+        const string code = """
+            using System.Collections.Generic;
+            using Robust.Shared.Serialization.Manager.Attributes;
+            using Robust.Shared.Prototypes;
+
+            public sealed class Foo
+            {
+                [ValidatePrototypeIdAttribute<object>]
+                public const string Good = "Foobar";
+
+                [ValidatePrototypeIdAttribute<object>]
+                public static readonly ProtoId<object> Bad = new("Foobar");
+
+                [ValidatePrototypeIdAttribute<object>]
+                public static readonly List<ProtoId<object>> BadCollection = [new("Foobar")];
+            }
+            """;
+
+        await Verifier(code,
+            // /0/Test0.cs(10,6): info RA0033: Redundant [ValidateProtoId<T>] attribute
+            VerifyCS.Diagnostic().WithSpan(10, 6, 10, 42),
+            // /0/Test0.cs(13,6): info RA0033: Redundant [ValidateProtoId<T>] attribute
+            VerifyCS.Diagnostic().WithSpan(13, 6, 13, 42));
+    }
+}
---
+++ Robust.Analyzers/ValidateProtoIdAnalyzer.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Robust.Roslyn.Shared;
+
+namespace Robust.Analyzers;
+
+#nullable enable
+
+/// <summary>
+/// Analyzer that detects redundant uses of <c>[ValidatePrototypeId&lt;T&gt;]</c>.
+/// </summary>
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class ValidateProtoIdAnalyzer : DiagnosticAnalyzer
+{
+    private const string ValidateProtoIdAttributeType =
+        "Robust.Shared.Serialization.Manager.Attributes.ValidatePrototypeIdAttribute`1";
+
+    private static readonly string[] TypedProtoIdTypes =
+    [
+        "Robust.Shared.Prototypes.EntProtoId",
+        "Robust.Shared.Prototypes.EntProtoId`1",
+        "Robust.Shared.Prototypes.ProtoId`1",
+    ];
+
+    private static readonly DiagnosticDescriptor RuleRedundantValidateAttribute = new(
+        Diagnostics.IdUnnecessaryValidateProtoId,
+        "Redundant [ValidatePrototypeId<T>] attribute",
+        "Redundant [ValidatePrototypeId<T>] attribute",
+        "Usage",
+        DiagnosticSeverity.Warning,
+        isEnabledByDefault: true,
+        "[ValidatePrototypeId<T>] is only necessary for fields static containing raw strings, not fields containing ProtoId<T> or similar types.");
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => [RuleRedundantValidateAttribute];
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.ConfigureGeneratedCodeAnalysis(
+            GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+        context.EnableConcurrentExecution();
+
+        context.RegisterCompilationStartAction(compilationContext =>
+        {
+            var validateProtoIdAttributeType =
+                compilationContext.Compilation.GetTypeByMetadataName(ValidateProtoIdAttributeType);
+            var typeIEnumerable =
+                compilationContext.Compilation.GetSpecialType(SpecialType.System_Collections_Generic_IEnumerable_T);
+            if (validateProtoIdAttributeType == null)
+                return;
+
+            INamedTypeSymbol[] badTypes = TypedProtoIdTypes
+                .Select(n => compilationContext.Compilation.GetTypeByMetadataName(n))
+                .Where(t => t != null)
+                .ToArray()!;
+
+            if (badTypes.Length == 0)
+                return;
+
+            compilationContext.RegisterSymbolAction(symbolContext =>
+                {
+                    if (symbolContext.Symbol is not IFieldSymbol field)
+                        return;
+
+                    if (!field.IsStatic)
+                        return;
+
+                    if (!HasValidateAttribute(field, validateProtoIdAttributeType, out var attr))
+                        return;
+
+                    if (field.Type is not INamedTypeSymbol fieldType)
+                        return;
+
+                    if (!HasBadFieldType(fieldType, badTypes))
+                    {
+                        if (GetSecondTypeForAnalysis(fieldType, typeIEnumerable) is { } second
+                            && !HasBadFieldType(second, badTypes))
+                            return;
+                    }
+
+                    var location = attr.ApplicationSyntaxReference?.GetSyntax().GetLocation() ?? field.Locations[0];
+                    symbolContext.ReportDiagnostic(Diagnostic.Create(RuleRedundantValidateAttribute, location));
+                },
+                SymbolKind.Field);
+        });
+    }
+
+    private static bool HasValidateAttribute(
+        IFieldSymbol field,
+        INamedTypeSymbol attributeType,
+        [NotNullWhen(true)] out AttributeData? foundAttr)
+    {
+        var attrs = field.GetAttributes();
+        foreach (var attr in attrs)
+        {
+            if (attr.AttributeClass is not { IsGenericType: true })
+                continue;
+
+            var baseGenericType = attr.AttributeClass.ConstructedFrom;
+            if (SymbolEqualityComparer.Default.Equals(baseGenericType, attributeType))
+            {
+                foundAttr = attr;
+                return true;
+            }
+        }
+
+        foundAttr = null;
+        return false;
+    }
+
+    private static bool HasBadFieldType(INamedTypeSymbol namedType, INamedTypeSymbol[] badSymbols)
+    {
+        if (namedType.IsGenericType)
+            namedType = namedType.ConstructedFrom;
+
+        foreach (var badSymbol in badSymbols)
+        {
+            if (SymbolEqualityComparer.Default.Equals(badSymbol, namedType))
+                return true;
+        }
+
+        return false;
+    }
+
+    private static INamedTypeSymbol? GetSecondTypeForAnalysis(INamedTypeSymbol type, INamedTypeSymbol typeIEnumerable)
+    {
+        foreach (var interfaceType in type.AllInterfaces)
+        {
+            if (interfaceType.IsGenericType &&
+                SymbolEqualityComparer.Default.Equals(interfaceType.ConstructedFrom, typeIEnumerable))
+            {
+                return interfaceType.TypeArguments[0] as INamedTypeSymbol;
+            }
+        }
+
+        return null;
+    }
+}
